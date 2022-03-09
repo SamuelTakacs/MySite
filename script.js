42a9b2caad97f7a9674a217acce3a10532a3a5a9
SamuelTakacs <98512622+SamuelTakacs@users.noreply.github.com>
2022-03-09 12:28:06 +0100

    a

--- script.js
+++ script.js
@@ -1,4 +1,87 @@
-function myFunction() {
-    var meal1c = document.getElementById("meal1s");
-    meal1c.classList.toggle("show");
+function meal1() {
+    var meal = document.getElementById("meal1s");
+    meal.classList.toggle("show");
   }
+function meal2() {
+    var meal = document.getElementById("meal2s");
+    meal.classList.toggle("show");
+  }
+function meal3() {
+    var meal = document.getElementById("meal3s");
+    meal.classList.toggle("show");
+  }
+function meal4() {
+    var meal = document.getElementById("meal4s");
+    meal.classList.toggle("show");
+  }
+function meal5() {
+    var meal = document.getElementById("meal5s");
+    meal.classList.toggle("show");
+  }
+function meal6() {
+    var meal = document.getElementById("meal6s");
+    meal.classList.toggle("show");
+  }
+function meal7() {
+    var meal = document.getElementById("meal7s");
+    meal.classList.toggle("show");
+  }
+// End of breakfast script for popup
+function meal8() {
+  var meal = document.getElementById("meal8s");
+  meal.classList.toggle("show");
+}
+function meal9() {
+  var meal = document.getElementById("meal9s");
+  meal.classList.toggle("show");
+}
+function meal10() {
+  var meal = document.getElementById("meal10s");
+  meal.classList.toggle("show");
+}
+function meal11() {
+  var meal = document.getElementById("meal11s");
+  meal.classList.toggle("show");
+}
+function meal12() {
+  var meal = document.getElementById("meal12s");
+  meal.classList.toggle("show");
+}
+function meal13() {
+  var meal = document.getElementById("meal13s");
+  meal.classList.toggle("show");
+}
+function meal14() {
+  var meal = document.getElementById("meal14s");
+  meal.classList.toggle("show");
+}
+//End of launch selection for popup
+function meal15() {
+  var meal = document.getElementById("meal15s");
+  meal.classList.toggle("show");
+}
+function meal16() {
+  var meal = document.getElementById("meal16s");
+  meal.classList.toggle("show");
+}
+function meal17() {
+  var meal = document.getElementById("meal17s");
+  meal.classList.toggle("show");
+}
+function meal18() {
+  var meal = document.getElementById("meal18s");
+  meal.classList.toggle("show");
+}
+function meal19() {
+  var meal = document.getElementById("meal19s");
+  meal.classList.toggle("show");
+}
+function meal20() {
+  var meal = document.getElementById("meal20s");
+  meal.classList.toggle("show");
+}
+function meal21() {
+  var meal = document.getElementById("meal21s");
+  meal.classList.toggle("show");
+}
+//End of dinner selection
