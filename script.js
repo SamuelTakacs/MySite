472edfc66bbf6d056666e2813e7b5dfaea34b03d
SamuelTakacs <98512622+SamuelTakacs@users.noreply.github.com>
2022-04-15 10:39:25 +0200

    added pizza algo

--- script.js
+++ script.js
@@ -1,56 +1,231 @@
+
 function pondelok() {
-    var x = document.getElementById("hid-pon");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    let uto = document.getElementById("hid-uto");
+    let str = document.getElementById("hid-str");
+    let stv = document.getElementById("hid-stv");
+    let pon = document.getElementById("hid-pon");
+    let pia = document.getElementById("hid-pia");
+    let sob = document.getElementById("hid-sob");
+    let ned = document.getElementById("hid-ned");
+    if (pon.style.display === "block") {
+        pon.style.display = "none";
     } else {
-        x.style.display = "block";
-    }
-}
+        pon.style.display = "block";
+    };
+    if (uto.style.display === 'block') {
+        uto.style.display = "none";
+    };
+    if (str.style.display === 'block') {
+        str.style.display = "none";
+    };
+    if (stv.style.display === 'block') {
+        stv.style.display = "none";
+    };
+    if (pia.style.display === 'block') {
+        pia.style.display = "none";
+    };
+    if (sob.style.display === 'block') {
+        sob.style.display = "none";
+    };
+    if (ned.style.display === 'block') {
+        ned.style.display = "none";
+    }; 
+};
+
 function utorok() {
-    var x = document.getElementById("hid-uto");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    let uto = document.getElementById("hid-uto");
+    let str = document.getElementById("hid-str");
+    let stv = document.getElementById("hid-stv");
+    let pon = document.getElementById("hid-pon");
+    let pia = document.getElementById("hid-pia");
+    let sob = document.getElementById("hid-sob");
+    let ned = document.getElementById("hid-ned");
+    if (uto.style.display === "block") {
+        uto.style.display = "none";
     } else {
-        x.style.display = "block";
-    }
-}
+        uto.style.display = "block";
+    };
+    if (pon.style.display === 'block') {
+        pon.style.display = "none";
+    };
+    if (str.style.display === 'block') {
+        str.style.display = "none";
+    };
+    if (stv.style.display === 'block') {
+        stv.style.display = "none";
+    };
+    if (pia.style.display === 'block') {
+        pia.style.display = "none";
+    };
+    if (sob.style.display === 'block') {
+        sob.style.display = "none";
+    };
+    if (ned.style.display === 'block') {
+        ned.style.display = "none";
+    }; 
+};
+
 function streda() {
-    var x = document.getElementById("hid-str");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    let uto = document.getElementById("hid-uto");
+    let str = document.getElementById("hid-str");
+    let stv = document.getElementById("hid-stv");
+    let pon = document.getElementById("hid-pon");
+    let pia = document.getElementById("hid-pia");
+    let sob = document.getElementById("hid-sob");
+    let ned = document.getElementById("hid-ned");
+    if (str.style.display === "block") {
+        str.style.display = "none";
     } else {
-        x.style.display = "block";
-    }
-}
+        str.style.display = "block";
+    };
+    if (uto.style.display === 'block') {
+        uto.style.display = "none";
+    };
+    if (pon.style.display === 'block') {
+        pon.style.display = "none";
+    };
+    if (stv.style.display === 'block') {
+        stv.style.display = "none";
+    };
+    if (pia.style.display === 'block') {
+        pia.style.display = "none";
+    };
+    if (sob.style.display === 'block') {
+        sob.style.display = "none";
+    };
+    if (ned.style.display === 'block') {
+        ned.style.display = "none";
+    }; 
+};
+
 function stvrtok() {
-    var x = document.getElementById("hid-stv");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    let uto = document.getElementById("hid-uto");
+    let str = document.getElementById("hid-str");
+    let stv = document.getElementById("hid-stv");
+    let pon = document.getElementById("hid-pon");
+    let pia = document.getElementById("hid-pia");
+    let sob = document.getElementById("hid-sob");
+    let ned = document.getElementById("hid-ned");
+    if (stv.style.display === "block") {
+        stv.style.display = "none";
     } else {
-        x.style.display = "block";
-    }
-}
+        stv.style.display = "block";
+    };
+    if (uto.style.display === 'block') {
+        uto.style.display = "none";
+    };
+    if (str.style.display === 'block') {
+        str.style.display = "none";
+    };
+    if (pon.style.display === 'block') {
+        pon.style.display = "none";
+    };
+    if (pia.style.display === 'block') {
+        pia.style.display = "none";
+    };
+    if (sob.style.display === 'block') {
+        sob.style.display = "none";
+    };
+    if (ned.style.display === 'block') {
+        ned.style.display = "none";
+    }; 
+};
+
 function piatok() {
-    var x = document.getElementById("hid-pia");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    let uto = document.getElementById("hid-uto");
+    let str = document.getElementById("hid-str");
+    let stv = document.getElementById("hid-stv");
+    let pon = document.getElementById("hid-pon");
+    let pia = document.getElementById("hid-pia");
+    let sob = document.getElementById("hid-sob");
+    let ned = document.getElementById("hid-ned");
+    if (pia.style.display === "block") {
+        pia.style.display = "none";
     } else {
-        x.style.display = "block";
-    }
-}
+        pia.style.display = "block";
+    };
+    if (uto.style.display === 'block') {
+        uto.style.display = "none";
+    };
+    if (str.style.display === 'block') {
+        str.style.display = "none";
+    };
+    if (stv.style.display === 'block') {
+        stv.style.display = "none";
+    };
+    if (pon.style.display === 'block') {
+        pon.style.display = "none";
+    };
+    if (sob.style.display === 'block') {
+        sob.style.display = "none";
+    };
+    if (ned.style.display === 'block') {
+        ned.style.display = "none";
+    }; 
+};
+
 function sobota() {
-    var x = document.getElementById("hid-sob");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    let uto = document.getElementById("hid-uto");
+    let str = document.getElementById("hid-str");
+    let stv = document.getElementById("hid-stv");
+    let pon = document.getElementById("hid-pon");
+    let pia = document.getElementById("hid-pia");
+    let sob = document.getElementById("hid-sob");
+    let ned = document.getElementById("hid-ned");
+    if (sob.style.display === "block") {
+        sob.style.display = "none";
     } else {
-        x.style.display = "block";
-    }
-}
+        sob.style.display = "block";
+    };
+    if (uto.style.display === 'block') {
+        uto.style.display = "none";
+    };
+    if (str.style.display === 'block') {
+        str.style.display = "none";
+    };
+    if (stv.style.display === 'block') {
+        stv.style.display = "none";
+    };
+    if (pia.style.display === 'block') {
+        pia.style.display = "none";
+    };
+    if (pon.style.display === 'block') {
+        pon.style.display = "none";
+    };
+    if (ned.style.display === 'block') {
+        ned.style.display = "none";
+    }; 
+};
+
 function nedela() {
-    var x = document.getElementById("hid-ned");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    let uto = document.getElementById("hid-uto");
+    let str = document.getElementById("hid-str");
+    let stv = document.getElementById("hid-stv");
+    let pon = document.getElementById("hid-pon");
+    let pia = document.getElementById("hid-pia");
+    let sob = document.getElementById("hid-sob");
+    let ned = document.getElementById("hid-ned");
+    if (ned.style.display === "block") {
+        ned.style.display = "none";
     } else {
-        x.style.display = "block";
-    }
-}
+        ned.style.display = "block";
+    };
+    if (uto.style.display === 'block') {
+        uto.style.display = "none";
+    };
+    if (str.style.display === 'block') {
+        str.style.display = "none";
+    };
+    if (stv.style.display === 'block') {
+        stv.style.display = "none";
+    };
+    if (pia.style.display === 'block') {
+        pia.style.display = "none";
+    };
+    if (sob.style.display === 'block') {
+        sob.style.display = "none";
+    };
+    if (pon.style.display === 'block') {
+        pon.style.display = "none";
+    }; 
+};
