0afe1f9f463cab3891940f0589b732acb3291ff0
samueltakacs <98512622+SamuelTakacs@users.noreply.github.com>
2022-06-17 13:32:43 +0200

    2.1.6

--- animation.js
+++ animation.js
@@ -1,3 +1,23 @@
 window.addEventListener('DOMContentLoaded',function() {
     document.body.setAttribute('class','visible');
+    document.getElementById('monday').setAttribute('class','day-div-v')
+    setTimeout(function(){
+        document.getElementById('tuesday').setAttribute('class','day-div-v')
+    },500)
+    setTimeout(function(){
+        document.getElementById('wednesday').setAttribute('class','day-div-v')
+    },1000)
+    setTimeout(function(){
+        document.getElementById('thursday').setAttribute('class','day-div-v')
+    },1500)
+    setTimeout(function(){
+        document.getElementById('friday').setAttribute('class','day-div-v')
+    },2000)
+    setTimeout(function(){
+        document.getElementById('saturday').setAttribute('class','day-div-v')
+    },2500)
+    setTimeout(function(){
+        document.getElementById('sunday').setAttribute('class','day-div-v')
+    },3000)
 })
+
